3074eddcee31f706562ea12654724d9975b973f8
Christian Liesch <liesch@gmx.ch>
2014-11-16 13:45:59 +0100

    First very simple example

--- Physics/Simple3D/Scripts/Main.lua
+++ Physics/Simple3D/Scripts/Main.lua
@@ -1,8 +1,24 @@
+-- Physics3d. basic 3d physics scene
 
-scene = Scene(Scene.SCENE_3D)
+-- initialise scene
+scene = PhysicsScene(60, Vector3(30,30,30), false)
 
-sceneEntity = SceneEntityInstance(scene, "Resources/scene.entity")
-scene:addChild(sceneEntity)
+-- add ground
+ground = ScenePrimitive(ScenePrimitive.TYPE_PLANE, 8,8)
+ground.rigidBody = true
+ground:loadTexture("Resources/green_texture.png")
+scene:addCollisionChild(ground, PhysicsEntity.SHAPE_PLANE, 1)
 
-camera = safe_cast(sceneEntity:getEntityById("main_camera", true), Camera)
-scene:setActiveCamera(camera)
+-- add 30 cubes
+for i=0,29 do
+   box =  ScenePrimitive(ScenePrimitive.TYPE_BOX, 1,1,1)
+   box:loadTexture("Resources/blue_texture.png")
+   box:setPosition( 2*random(), (8.0 + (16*random())), 2*random() )
+--   scene:setSpin(box, vector3(,random(),random()) )
+   box.rigidBody = true
+   scene:addPhysicsChild(box, PhysicsEntity.SHAPE_BOX, 1, 0.5, 0, 1, false)
+end
+
+scene:getDefaultCamera():setPosition(7,7,7)
+scene:getDefaultCamera():lookAt(Vector3(0,0,0), Vector3(0,1,0))
+
