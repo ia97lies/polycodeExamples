f0124a757da88991df2f7e46389d1a5e0b83c8b7
Christian Liesch <liesch@gmx.ch>
2014-11-17 23:07:51 +0100

    played around with quaterion and stuff

--- Graphics/3D/OrbitCamera/Scripts/Main.lua
+++ Graphics/3D/OrbitCamera/Scripts/Main.lua
@@ -14,8 +14,8 @@ scene:addEntity(ground)
 camera = scene:getDefaultCamera()
 camera.private = {}
 camera.private.upVector = Vector3(0,1,0)
-camera.private.jaw = 0
-camera.private.pitch = 0
+camera.private.yaw = 0
+camera.private.pitch = 20
 camera.private.distance = 15 
 
 camera:setPosition(7,7,7)
@@ -29,17 +29,36 @@ function onKeyDown(key)
   end
 end
 
-function Update(elapsed)
-  local rotation = Quaternion(camera.private.jaw, camera.private.upVector.x, camera.private.upVector.y, camera.private.upVector.z)
-  local cameraPosition = Vector3(0, 0, 0);
-  cameraPosition.y = cameraPosition.y - camera.private.distance;
-  forward = Vector3(cameraPosition.x, cameraPosition.y, cameraPosition.z);
+function cameraUpdate()
+  local pitch = camera.private.pitch * RADIANS_PER_DEGREE
+  local yaw = camera.private.yaw * RADIANS_PER_DEGREE
+
+  local rotation = Quaternion(camera.private.upVector.x, camera.private.upVector.y, camera.private.upVector.z, yaw)
+  local cameraPosition = Vector3(1, 0, 1)
+  cameraPosition.y = cameraPosition.y - camera.private.distance
+  forward = Vector3(cameraPosition.x, cameraPosition.y, cameraPosition.z)
   if (forward:length() < 1) then 
-    forward.set(1, 0, 0);
+    forward.set(1, 0, 0)
   end 
+  print(cameraPosition.x)
+  print(cameraPosition.y)
+  print(cameraPosition.z)
 
+  local rigth = camera.private.upVector:crossProduct(forward)
+  local roll = Quaternion(rigth.x, rigth.y, rigth.z, pitch);
 
-  camera:setPosition(7,7,7)
+  cameraPosition = rotation:applyTo(cameraPosition)
+  cameraPosition = roll:applyTo(cameraPosition)
+
+  --camera:setPosition(7,7,7)
+  print(cameraPosition.x)
+  print(cameraPosition.y)
+  print(cameraPosition.z)
+  camera:setPosition(cameraPosition.x, cameraPosition.y, cameraPosition.z)
   camera:lookAt(Vector3(0,0,0), camera.private.upVector)
 end
 
+function Update(elapsed)
+end
+
+cameraUpdate()
