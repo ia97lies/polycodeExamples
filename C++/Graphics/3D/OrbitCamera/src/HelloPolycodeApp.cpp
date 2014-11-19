28ffe53de9719c7e787145ee27cc607d6be687c9
Christian Liesch <liesch@gmx.ch>
2014-11-19 22:15:59 +0100

    Use code from the book and adapt it for polycode

--- C++/Graphics/3D/OrbitCamera/src/HelloPolycodeApp.cpp
+++ C++/Graphics/3D/OrbitCamera/src/HelloPolycodeApp.cpp
@@ -5,7 +5,7 @@
 
 #include "HelloPolycodeApp.h"
 
-HelloPolycodeApp::HelloPolycodeApp(PolycodeView *view) {
+HelloPolycodeApp::HelloPolycodeApp(PolycodeView *view) : EventHandler() {
 
 	core = new POLYCODE_CORE(view, 640,480,false,true,0,0,90, 0, true);
 
@@ -25,6 +25,12 @@ HelloPolycodeApp::HelloPolycodeApp(PolycodeView *view) {
 	scene->getDefaultCamera()->setPosition(7,7,7);
 	scene->getDefaultCamera()->lookAt(Vector3(0,0,0));
 
+  core->getInput()->addEventListener(this, InputEvent::EVENT_KEYDOWN);
+  core->getInput()->addEventListener(this, InputEvent::EVENT_KEYUP);
+
+  pitch = 20.0f;
+  yaw = 0.0f;
+  distance = 15.0f;
 }
 
 HelloPolycodeApp::~HelloPolycodeApp() {
@@ -38,29 +44,105 @@ void HelloPolycodeApp::handleEvent(Event *e) {
 			case InputEvent::EVENT_KEYDOWN:
 				switch (inputEvent->keyCode()) {
 					case KEY_LEFT:
-            fprintf(stderr, "Key left pressed\n");
+            rotateCamera(yaw, +CAMERA_STEP_SIZE); 
+            fprintf(stderr, "yaw: %f\n", yaw);
             fflush(stderr);
 					break;
 					case KEY_RIGHT:
-            fprintf(stderr, "Key left pressed\n");
+            rotateCamera(yaw, -CAMERA_STEP_SIZE);
+            fprintf(stderr, "yaw: %f\n", yaw);
             fflush(stderr);
 					break;
+					case KEY_UP:
+            rotateCamera(pitch, +CAMERA_STEP_SIZE);
+            fprintf(stderr, "pitch: %f\n", pitch);
+            fflush(stderr);
+					break;
+					case KEY_DOWN:
+            rotateCamera(pitch, -CAMERA_STEP_SIZE);
+            fprintf(stderr, "pitch: %f\n", pitch);
+            fflush(stderr);
+					break;
+
 				}
 			break;
-			case InputEvent::EVENT_KEYUP:
-				switch (inputEvent->key) {
-					case KEY_LEFT:
-					case KEY_RIGHT:
-            fprintf(stderr, "Key released\n");
-            fflush(stderr);
-					break;					
-				}
-			break;			
 		}
 		
 	}
 }
 
+void HelloPolycodeApp::rotateCamera(float &angle, float value) {
+  angle -= value; 
+  if (angle < 0) angle += 360; 
+  if (angle >= 360) angle -= 360;
+}
+
+
+void HelloPolycodeApp::cameraUpdate() {
+  // our values represent the angles in degrees, but 3D 
+  // math typically demands angular values are in radians.
+  float pitchRad = pitch * RADIANS_PER_DEGREE;
+  float yawRad = yaw * RADIANS_PER_DEGREE;
+
+  // create a quaternion defining the angular rotation 
+  // around the up vector
+  Quaternion rotation(yawRad, 0.0f, 1.0f, 0.0f);
+  Vector3 position(0.0f, 0.0f, -distance);
+
+#if 0
+  // set the camera's position to 0,0,0, then move the 'z' 
+  // position to the current value of m_cameraDistance.
+  btVector3 cameraPosition(0,0,0);
+  cameraPosition[2] = -m_cameraDistance;
+
+  fprintf(stderr, "cameraTarget x:%f, y:%f, z:%f\n", m_cameraTarget[0], m_cameraTarget[1], m_cameraTarget[2]);
+  fprintf(stderr, "cameraPos x:%f, y:%f, z:%f\n", cameraPosition[0], cameraPosition[1], cameraPosition[2]);
+
+  // create a Bullet Vector3 to represent the camera 
+  // position and scale it up if its value is too small.
+  btVector3 forward(cameraPosition[0], cameraPosition[1], cameraPosition[2]);
+  if (forward.length2() < SIMD_EPSILON) {
+    forward.setValue(1.f,0.f,0.f);
+  }
+
+  // figure out the 'right' vector by using the cross 
+  // product on the 'forward' and 'up' vectors
+  btVector3 right = m_upVector.cross(forward);
+
+  // create a quaternion that represents the camera's roll
+  btQuaternion roll(right, - pitchRad);
+
+  // turn the rotation (around the Y-axis) and roll (around 
+  // the forward axis) into transformation matrices and 
+  // apply them to the camera position. This gives us the 
+  // final position
+  cameraPosition = btMatrix3x3(rotation) * btMatrix3x3(roll) * cameraPosition;
+  fprintf(stderr, "newCameraPos x:%f, y:%f, z:%f\n", cameraPosition[0], cameraPosition[1], cameraPosition[2]);
+
+  // save our new position in the member variable, and 
+  // shift it relative to the target position (so that we 
+  // orbit it)
+  m_cameraPosition[0] = cameraPosition.getX();
+  m_cameraPosition[1] = cameraPosition.getY();
+  m_cameraPosition[2] = cameraPosition.getZ();
+  m_cameraPosition += m_cameraTarget;
+
+  // create a view matrix based on the camera's position and where it's
+  // looking
+  gluLookAt(m_cameraPosition[0], 
+            m_cameraPosition[1], 
+            m_cameraPosition[2], 
+            m_cameraTarget[0], 
+            m_cameraTarget[1], 
+            m_cameraTarget[2], 
+            m_upVector.getX(), 
+            m_upVector.getY(), 
+            m_upVector.getZ());
+  // the view matrix is now set
+#endif
+}
+
 bool HelloPolycodeApp::Update() {
     return core->updateAndRender();
 }
+
--- C++/Graphics/3D/OrbitCamera/src/HelloPolycodeApp.h
+++ C++/Graphics/3D/OrbitCamera/src/HelloPolycodeApp.h
@@ -8,7 +8,10 @@
 
 using namespace Polycode;
 
-class HelloPolycodeApp {
+#define RADIANS_PER_DEGREE 0.01745329f
+#define CAMERA_STEP_SIZE 5.0f
+
+class HelloPolycodeApp : public EventHandler {
 public:
     HelloPolycodeApp(PolycodeView *view);
     ~HelloPolycodeApp();
@@ -17,4 +20,10 @@ public:
     
 private:
 	Core *core;
+  float pitch;
+  float yaw;
+  float distance;
+
+  void rotateCamera(float &angle, float value);
+  void cameraUpdate(); 
 };
