35da5151ad949761114241a231a37c8fffa6baeb
Christian Liesch <liesch@gmx.ch>
2014-11-22 09:37:11 +0100

    Polycode textures

--- Lua/Physics/3D/Simple/Scripts/Main.lua
+++ Lua/Physics/3D/Simple/Scripts/Main.lua
@@ -12,7 +12,7 @@ scene:addCollisionChild(ground, PhysicsEntity.SHAPE_PLANE, 1)
 -- add 30 cubes
 for i=0,29 do
    box =  ScenePrimitive(ScenePrimitive.TYPE_BOX, 1,1,1)
-   box:loadTexture("Resources/blue_texture.png")
+   box:loadTexture("Resources/pink_texture.png")
    box:setPosition( 2*random(), (8.0 + (16*random())), 2*random() )
 --   scene:setSpin(box, vector3(,random(),random()) )
    box.rigidBody = true
