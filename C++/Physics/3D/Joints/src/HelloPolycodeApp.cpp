ad5f9818ab01d1bb6ea4ec138d7b1e5f2237ec80
ia97lies <liesch@gmx.ch>
2015-03-07 13:54:42 +0100

    adjust the simple joint example

--- C++/Physics/3D/Joints/src/HelloPolycodeApp.cpp
+++ C++/Physics/3D/Joints/src/HelloPolycodeApp.cpp
@@ -1,3 +1,4 @@
+#include "unistd.h"
 #include "HelloPolycodeApp.h"
 
 HelloPolycodeApp::HelloPolycodeApp(PolycodeView *view) {
@@ -15,23 +16,26 @@ HelloPolycodeApp::HelloPolycodeApp(PolycodeView *view) {
 
   ScenePrimitive *box1 = new ScenePrimitive(ScenePrimitive::TYPE_BOX, 0.5,0.5,0.5);
   box1->loadTexture("Resources/pink_texture.png");
-  box1->setPosition(0,1,0);
-  scene->addPhysicsChild(box1, PhysicsSceneEntity::SHAPE_BOX, 1.0);
+  box1->setPosition(0,4,0);
+  scene->addEntity(box1);
 
   ScenePrimitive *box2 = new ScenePrimitive(ScenePrimitive::TYPE_BOX, 0.5,0.5,0.5);
   box2->loadTexture("Resources/pink_texture.png");
   box2->setPosition(1,0,0);
   box1->addChild(box2);
+
+  scene->trackPhysicsChild(box1, PhysicsSceneEntity::SHAPE_BOX, 1.0);
   scene->trackPhysicsChild(box2, PhysicsSceneEntity::SHAPE_BOX, 1.0);
 
+  PhysicsHingeConstraint *constraint1 = scene->createHingeConstraint(box1, Vector3(0,0,0), Vector3(0,1,0), 0, 0);
   // PhysicsHingeConstraint *createHingeJoint(Entity *entity1, Entity *entity2, Vector3 pivot1, Vector3 axis1, Vector3 pivot2, Vector3 axis2, Number minLimit, Number maxLimit);
-  PhysicsHingeConstraint *constrain = scene->createHingeJoint(box1, box2, Vector3(0,0,0), Vector3(0,1,0), Vector3(-1,0,0), Vector3(0,1,0), 0, 120 * 3.14159265 / 180);
-  constrain->btConstraint->enableAngularMotor(true, 1, 1);
+  PhysicsHingeConstraint *constraint2 = scene->createHingeJoint(box1, box2, Vector3(0,0,0), Vector3(0,1,0), Vector3(-1,0,0), Vector3(0,1,0), 0, /*165 * 3.14159265 / 180*/100);
+  constraint2->btConstraint->enableAngularMotor(true, 1, 1);
 
+  //scene->removeConstraint(constraint1);
 
 	scene->getDefaultCamera()->setPosition(7,7,7);
 	scene->getDefaultCamera()->lookAt(Vector3(0,0,0));
-	
 }
 
 HelloPolycodeApp::~HelloPolycodeApp() {
